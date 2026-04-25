44f0600eaf6b3bc4d2b945e81524314330b3d2c5
LOSMARRY <marioabras137@gmail.com>
2026-04-25 12:07:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(
-    api_key=st.secrets["GEMINI_CHAVE"],
-    transport="rest"
-)
+# CONFIG
+genai.configure(api_key=st.secrets["GEMINI_CHAVE"])
 
 st.title("Teste Gemini")
 
 try:
-    model = genai.GenerativeModel("models/gemini-1.0-pro")
-    response = model.generate_content("diga oi")
-
-    st.success("FUNCIONOU")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    
+    response = model.generate_content("Diga apenas: funcionando")
+    
+    st.success("✅ Funcionou!")
     st.write(response.text)
 
 except Exception as e:
-    st.error(e)
+    st.error(f"Erro: {e}")
